ba6e10dc1fc07eae20f9977c5308785a8ee3b222
Raja <kingraja673@gmail.com>
2022-09-23 19:09:50 +0530

    secure text entry added

--- client/src/components/RegisterComponent/RegisterComponent.js
+++ client/src/components/RegisterComponent/RegisterComponent.js
@@ -1,16 +1,23 @@
-import {View, Text, Image, Pressable, Alert,ScrollView,SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  Pressable,
+  Alert,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
 import React, {useState} from 'react';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import Input from '../common/Input/Input';
 import CustomButton from './../common/CustomButton/CustomButton';
 import Container from './../common/container/Container';
 import {text} from '../../styles/Global';
 import styles from './styles';
-import { ROUTE_NAMES } from '../../constants/routeNames';
-const RegisterComponent = ({onChange,form,onSubmit,errors,onClear}) => {
-  const {navigate} = useNavigation()
+import {ROUTE_NAMES} from '../../constants/routeNames';
+const RegisterComponent = ({onChange, form, onSubmit, errors, onClear}) => {
+  const {navigate} = useNavigation();
 
-  
   return (
     <>
       <Container>
@@ -23,74 +30,67 @@ const RegisterComponent = ({onChange,form,onSubmit,errors,onClear}) => {
         <Text style={text.primaryText}>Welcome To Heartly</Text>
         <Input
           label="FirstName"
-          onChangeText={(value)=>onChange({name:'firstName',value})}
+          onChangeText={value => onChange({name: 'firstName', value})}
           value={form.firstName}
           error={errors.firstName}
         />
         <Input
           label="LastName"
-          onChangeText={(value)=>onChange({name:'lastName',value})}
+          onChangeText={value => onChange({name: 'lastName', value})}
           value={form.lastName}
           error={errors.lastName}
-
         />
         <Input
           label="Email"
-          onChangeText={(value)=>onChange({name:'email',value})}
+          onChangeText={value => onChange({name: 'email', value})}
           value={form.email}
           error={errors.email}
-
         />
         <Input
           label="Mobile"
-          onChangeText={(value)=>onChange({name:'mobile',value})}
+          onChangeText={value => onChange({name: 'mobile', value})}
           value={form.mobile}
           error={errors.mobile}
-
+          keyboardType="numeric"
+          maxLength={10}
         />
         <Input
           label="City"
-          onChangeText={(value)=>onChange({name:'city',value})}
+          onChangeText={value => onChange({name: 'city', value})}
           value={form.city}
           error={errors.city}
-
         />
         <Input
           label="Password"
-          onChangeText={(value)=>onChange({name:'password',value})}
+          onChangeText={value => onChange({name: 'password', value})}
           value={form.password}
           error={errors.password}
-
+          secureTextEntry={true}
         />
         <Input
           label="Confirm Password"
-          onChangeText={(value)=>onChange({name:'confirmPassword',value})}
+          onChangeText={value => onChange({name: 'confirmPassword', value})}
           value={form.confirmPassword}
           error={errors.confirmPassword}
-
+          secureTextEntry={true}
         />
         <CustomButton
           loading={false}
-          disabled={!(form.confirmPassword)}
+          disabled={!form.confirmPassword}
           title="SignUp"
           onPress={onSubmit}
           primary
         />
-         <CustomButton
-          loading={false}
-          title="Clear"
-          primary
-          onPress={onClear}
-        />
-          <View style={styles.createSection}>
-            <Text style={styles.infoText}>Already have an account?</Text>
-            <Pressable
-              onPress={() => {
-                navigate(ROUTE_NAMES.LOGIN)
-              }}>
-              <Text style={styles.linkBtn}>Login</Text>
-            </Pressable>
-          </View>
+        <CustomButton loading={false} title="Clear" primary onPress={onClear} />
+        <View style={styles.createSection}>
+          <Text style={styles.infoText}>Already have an account?</Text>
+          <Pressable
+            onPress={() => {
+              navigate(ROUTE_NAMES.LOGIN);
+            }}>
+            <Text style={styles.linkBtn}>Login</Text>
+          </Pressable>
+        </View>
       </Container>
     </>
   );
