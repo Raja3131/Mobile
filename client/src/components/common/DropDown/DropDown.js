23799b9f211df97f64f5fda0eb47c27cb890de62
Raja <kingraja673@gmail.com>
2022-09-30 19:08:24 +0530

    date picker installed

---
+++ client/src/components/common/DropDown/DropDown.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+
+const DropDown = () => {
+  return (
+    <View>
+      <Text>DropDown</Text>
+    </View>
+  )
+}
+
+export default DropDown
--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,8 +1,10 @@
 import {useContext, useEffect} from 'react';
-import {View, Text, Pressable} from 'react-native';
+import {View, Text, Pressable, Button} from 'react-native';
 import {GlobalContext} from '../../context/Provider';
 import styles from './styles';
 import { TextStyles } from '../../styles/Global';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+
 export const Home = () => {
   const {
     authDispatch,
@@ -12,6 +14,31 @@ export const Home = () => {
   const onLogOut = () => {
     logoutUser()(authDispatch);
 
+  };
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    if (Platform.OS === 'android') {
+      setShow(false);
+      // for iOS, add a button that closes the picker
+    }
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
   };
 
   return (
@@ -28,6 +55,21 @@ export const Home = () => {
        <Text style={[TextStyles.primaryText,{color:'black'}]}>Welcome To Heartly</Text>
        <Text style={[TextStyles.secondaryText,{color:'white'}]}>Hi {data['user'].firstName.toUpperCase()}</Text>
        </View>
+
+       <View>
+      <Button onPress={showDatepicker} title="Show date picker!" />
+      <Button onPress={showTimepicker} title="Show time picker!" />
+      <Text>selected: {date.toLocaleString()}</Text>
+      {show && (
+        <RNDateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+    </View>
       
       </View>
     </>
